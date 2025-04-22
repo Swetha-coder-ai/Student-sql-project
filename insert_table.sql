68f5e277e10fdd9a2b424563798bae9aa828a34c
Swetha Y <lyuvaraj198@gmail.com>
2025-04-22 15:10:53 +0530

    Create insert_table.sql

---
+++ insert_table.sql
@@ -0,0 +1,20 @@
+INSERT INTO Students VALUES
+(1, 'Aditi', '12A'),
+(2, 'Manoj', '12A'),
+(3, 'Rahul', '12B');
+
+INSERT INTO Subjects VALUES
+(101, 'Maths'),
+(102, 'English'),
+(103, 'Physics');
+
+INSERT INTO Marks VALUES
+(1, 101, 90),
+(1, 102, 85),
+(1, 103, 92),
+(2, 101, 70),
+(2, 102, 75),
+(2, 103, 80),
+(3, 101, 60),
+(3, 102, 50),
+(3, 103, 40);
